68eb0c4da3d491d197106704830bd28bdf474993
Akash Sharma <akashsharmaa7492@gmail.com>
2026-06-15 16:23:10 +0530

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -27,7 +27,7 @@ function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://scheme-finder-n4b7.onrender.com/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
