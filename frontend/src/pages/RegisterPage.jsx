6cd0b8312bcc37b3385b106e528a5b5dcdbafe97
Akash Sharma <akashsharmaa7492@gmail.com>
2026-06-15 16:16:20 +0530

    Update RegisterPage.jsx

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -28,7 +28,7 @@ function RegisterPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/register", {
+      const res = await fetch("https://scheme-finder-n4b7.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
